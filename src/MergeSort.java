be35faa5fc6bbde7d4500002727d9b7385320aae
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-06 11:48:27 +0530

    Added the In-Place Merge Sort Array method

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class MergeSort {
 
 
+    // TIme complexity is O(N*log2N)
+    // Space complexity is O(N)
     public static int[] MergeSorting(int[] array){
 
         // Use the copy of Range function to copy those elements which are required not the whole array and return the array
         // when only one element is remaining
+        // This is not in-place Merge Sort as we are making a new object every time
         if(array.length==1)
             return array;
 
@@ -61,6 +64,59 @@ public class MergeSort {
         return finalArray;
     }
 
+    public static void MergeSortingInPlace(int[] array, int start, int end){
+
+        if(end - start == 0)
+            return;
+
+        int mid= start + ( end-start)/2;
+
+        MergeSortingInPlace(array, start,mid);
+        //System.out.println("array: "+ Arrays.toString(array)+" Start:  "+start+" End: "+ end+ " Mid: "+mid);
+        MergeSortingInPlace(array,mid+1,end);
+        //System.out.println("array: "+ Arrays.toString(array)+" Start:  "+start+" End: "+ end+ " Mid: "+mid);
+        mergeInPlace(array,start,mid,end);
+    }
+
+    public static void  mergeInPlace(int[] array, int start, int mid , int end) {
+
+        int[] new_Array = new int[end - start+1];
+
+        int i = start;
+        int j = mid+1;
+        int k = 0;
+
+        while (i <= mid && j <= end){
+            if (array[i] < array[j]) {
+                new_Array[k] = array[i];
+                i++;
+            } else {
+                new_Array[k] = array[j];
+                j++;
+            }
+        k++;
+    }
+        while(i<=mid){
+            new_Array[k]=array[i];
+            k++;
+            i++;
+        }
+
+        while(j<=end){
+            new_Array[k]=array[j];
+            k++;
+            j++;
+        }
+
+        // Now saving it to the same array
+        int t=start;
+        for(int c =0 ;c<new_Array.length;c++){
+            array[t]=new_Array[c];
+            t++;
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Enter the Size of the array: ");
         Scanner scan= new Scanner(System.in);
@@ -82,5 +138,14 @@ public class MergeSort {
         int[] SArray= MergeSorting(array);
 
         System.out.println("\nSorted Array after Merge Sort is: "+ Arrays.toString(SArray));
+
+        // In Place Merge Sort
+        System.out.println("In Place Merge Sort: ");
+        int start=0;
+        int end=array.length-1;
+        MergeSortingInPlace(array,start,end);
+
+        System.out.println("Sorted Array after Merge Sort is: "+ Arrays.toString(array));
+
     }
 }
